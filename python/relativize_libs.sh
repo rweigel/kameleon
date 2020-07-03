6bfe0abd9816ee6ff0455ed649689efb1565bb66
rweigel <rweigel@gmu.edu>
2020-07-03 15:46:53 -0400

    update makefile

--- python/relativize_libs.sh
+++ python/relativize_libs.sh
@@ -12,12 +12,33 @@ mkdir -p ./lib/ #<copy the libraries here
 
 #join <(ldd "$1" |awk '{if(substr($3,0,1)=="/") print $1,$3}' |sort) <(patchelf --print-needed "$1" |sort) |cut -d\  -f2 | xargs -d '\n' -I{} cp --copy-contents {} ./lib
 
+echo "------"
+echo "Listed"
+echo "------"
+ldd "$1" | awk '{if(substr($3,0,2)=="/") print $1,$3}' | sort
+echo "------"
+echo "Needed"
+echo "------"
+patchelf --print-needed "$1" | sort
+
+echo "------"
+echo "Needed"
+echo "------"
+
+echo "------"
+echo "Result"
+echo "------"
+join <(ldd "$1" | awk '{if(substr($3,0,2)=="/") print $1,$3}' | sort) <(patchelf --print-needed "$1" | sort) | cut -d\  -f2
+
 # Modification ($3,0,1) -> ($3,0,2)
-join <(ldd "$1" | awk '{if(substr($3,0,2)=="/") print $1,$3}' | sort) <(patchelf --print-needed "$1" | sort) | cut -d\  -f2 | xargs -d '\n' -I{} cp --copy-contents {} ./lib 
+join <(ldd "$1" | awk '{if(substr($3,0,2)=="/") print $1,$3}' | sort) <(patchelf --print-needed "$1" | sort) | cut -d\  -f2 | xargs -d '\n' -I{} cp --copy-contents -n {} ./lib
+
+#ldd "$1" | awk '{if(substr($3,0,2)=="/") print $1,$3}' | cut -d\  -f2 | xargs -d '\n' -I{} cp --copy-contents -n {} .
 
 #make the relative lib paths override the system lib path
 # Original:
 #patchelf --set-rpath "\$ORIGIN/." "$1"
 # Modification:
-cp "$1" lib/
-cd lib/; patchelf --set-rpath "\$ORIGIN/." "$1"
+#cp "$1" lib/
+#cd lib/; patchelf --set-rpath "\$ORIGIN/." "$1"
+
