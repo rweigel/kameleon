8da7c57c354b13c1e8c83dc00fbd16270eb1e227
User <user@lubuntu1804-vm.fios-router.home>
2020-07-06 16:51:25 -0400

    update from ubuntu 18.04 build

--- python/kameleonV_test.py
+++ python/kameleonV_test.py
@@ -2,14 +2,15 @@ import os
 import sys
 import kameleonV
 
-print("------------------------------------------------------------------------------")
+print("---------------------------------------------------------------------------")
 print("Testing kameleonV in Python " + sys.version.replace("\n", ""))
-print("------------------------------------------------------------------------------")
+print("---------------------------------------------------------------------------")
 
 fname = '3d__var_1_e20120723-120000-000.out.cdf'
 fpath = '/tmp/' + fname
 
-url = 'http://mag.gmu.edu/git-data/bcurtiswx/Differences/data/Sean_Blake_081318_1/GM_CDF/'
+url = 'http://mag.gmu.edu/git-data/bcurtiswx/Differences/data/'
+url = url + 'Sean_Blake_081318_1/GM_CDF/'
 if not os.path.exists(fpath):
     print('Downloading ' + fname)
     os.system('cd /tmp; wget ' + url + fname)
@@ -25,7 +26,7 @@ except:
     print("Numpy not found. Not performing Numpy test.")
     assert(x1 == [0.009664899669587612, 0.44393426179885864])
     print('\033[92m' + "PASS" + '\033[0m')
-    print("------------------------------------------------------------------------------")
+    print("------------------------------------------------------------------------")
     sys.exit(0)
     
 x = np.array(x)
@@ -36,5 +37,5 @@ x2 = kameleonV.interpolate(fpath, x, y, z, 'p')
 assert(np.all(np.array(x1) == x2))
 
 print('\033[92m' + "PASS" + '\033[0m')
-print("------------------------------------------------------------------------------")
+print("----------------------------------------------------------------------------")
 sys.exit(0)
--- python/relativize_libs.sh
+++ python/relativize_libs.sh
@@ -3,7 +3,7 @@
 # Based on https://unix.stackexchange.com/a/289896
 
 #[ -n "$1" ] || set -- a.out
-mkdir -p ./lib/ #<copy the libraries here
+mkdir -p "$2" # copy the libraries here
 
 #use ldd to resolve the libs and use `patchelf --print-needed to
 # filter out "magic" libs kernel-interfacing libs such as
@@ -13,27 +13,21 @@ mkdir -p ./lib/ #<copy the libraries here
 #join <(ldd "$1" |awk '{if(substr($3,0,1)=="/") print $1,$3}' |sort) <(patchelf --print-needed "$1" |sort) |cut -d\  -f2 | xargs -d '\n' -I{} cp --copy-contents {} ./lib
 
 echo "------"
-echo "Listed"
+echo "Listed by ldd $1"
 echo "------"
 ldd "$1" | awk '{if(substr($3,0,2)=="/") print $1,$3}' | sort
 echo "------"
-echo "Needed"
+echo "Needed according to patchelf --print-needed $1"
 echo "------"
 patchelf --print-needed "$1" | sort
 
 echo "------"
-echo "Needed"
+echo "Copying these files to $2"
 echo "------"
-
-echo "------"
-echo "Result"
-echo "------"
+# Modification ($3,0,1) -> ($3,0,2)
 join <(ldd "$1" | awk '{if(substr($3,0,2)=="/") print $1,$3}' | sort) <(patchelf --print-needed "$1" | sort) | cut -d\  -f2
 
-# Modification ($3,0,1) -> ($3,0,2)
-#join <(ldd "$1" | awk '{if(substr($3,0,2)=="/") print $1,$3}' | sort) <(patchelf --print-needed "$1" | sort) | cut -d\  -f2 | xargs -d '\n' -I{} cp --copy-contents -n {} ./lib
-
-ldd "$1" | awk '{if(substr($3,0,2)=="/") print $1,$3}' | cut -d\  -f2 | xargs -d '\n' -I{} cp --copy-contents -n {} lib/
+join <(ldd "$1" | awk '{if(substr($3,0,2)=="/") print $1,$3}' | sort) <(patchelf --print-needed "$1" | sort) | cut -d\  -f2 | xargs -d '\n' -I{} cp --copy-contents -n {} ./lib
 
 #make the relative lib paths override the system lib path
 # Original:
